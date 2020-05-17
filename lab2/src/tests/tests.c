6aed6080491747763bb774a0b149f7aaa56f3b83
vestrik <vestrik11@gmail.com>
2020-05-17 19:45:42 +0000

    2

--- lab2/src/tests/tests.c
+++ lab2/src/tests/tests.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "revert_string.h"
+#include "/projects/vestrik-os/lab2/src/revert_string/revert_string.h"
 
 void testRevertString(void) {
   char simple_string[] = "Hello";

48ba9bddfd45c1fa300d1bc80728913f87ad8c25
vestrik <vestrik11@gmail.com>
2020-06-18 12:07:08 +0000

    tcp

--- lab7/src/tcpclient.c
+++ lab7/src/tcpclient.c
@@ -7,13 +7,74 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define BUFSIZE 100
+#include <stdbool.h>
+#include <getopt.h>
+//#define BUFSIZE 100
 #define SADDR struct sockaddr
 #define SIZE sizeof(struct sockaddr_in)
 
 int main(int argc, char *argv[]) {
   int fd;
   int nread;
+  
+  int BUFSIZE = -1;	
+  int SERV_PORT = -1;
+  char* ADDR;
+
+  while (true) {
+	    int current_optind = optind ? optind : 1;
+	
+	    static struct option options[] = {{"bufsize", required_argument, 0, 0},
+	                                      {"serv_port", required_argument, 0, 0},
+	                                      {"addr", required_argument, 0, 0},
+	                                      {0, 0, 0, 0}};
+	
+	    int option_index = 0;
+	    int c = getopt_long(argc, argv, "",options, &option_index);
+	
+	    if (c == -1) break;
+	
+	    switch (c) {
+	      case 0:
+	        switch (option_index) {
+	          case 0:
+	            BUFSIZE = atoi(optarg);
+	            if (BUFSIZE <= 0) {
+	                printf("bufsize must be a positive number\n");
+	                return 1;
+	              }
+	            break;
+	            case 1:
+	            SERV_PORT = atoi(optarg);
+	            if (SERV_PORT <= 0) {
+	                printf("serv_port must be a positive number\n");
+	                return 1;
+	            }
+	            break;
+	            case 2:
+	            ADDR = optarg;
+                if (ADDR <= 0) {
+	                printf("addr must be a positive number\n");
+	                return 1;
+	            }
+	            break;
+	        }
+	
+	      case '?':
+	        break;
+	
+	      default:
+	        printf("getopt returned character code 0%o?\n", c);
+	    }
+	
+	  }
+	  if (BUFSIZE == -1|| SERV_PORT == -1) {
+	    printf("Usage: %s --bufsize \"buffer_size\" --serv_port \"serv_port\" --addr \"addr\"\n",
+	           argv[0]);
+	    return 1;
+	  }
+
+
   char buf[BUFSIZE];
   struct sockaddr_in servaddr;
   if (argc < 3) {
@@ -29,12 +90,12 @@ int main(int argc, char *argv[]) {
   memset(&servaddr, 0, SIZE);
   servaddr.sin_family = AF_INET;
 
-  if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
-    perror("bad address");
+  if (inet_pton(AF_INET, ADDR, &servaddr.sin_addr) <= 0) {
+    perror("bad address");    
     exit(1);
   }
 
-  servaddr.sin_port = htons(atoi(argv[2]));
+  servaddr.sin_port = htons(SERV_PORT);
 
   if (connect(fd, (SADDR *)&servaddr, SIZE) < 0) {
     perror("connect");
